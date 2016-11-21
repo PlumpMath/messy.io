47cc0f075f62d76dc74d9c5a7bd4d772340ad057
Dan Taeyoung <provolot@gmail.com>
2016-11-21 03:06:03 -0500

    logintesT

---
+++ _DEVELOPMENT/messybot/logintest.py
@@ -0,0 +1,3 @@
+import PyISY
+client = PyISY.ISY("74.113.161.154", "1080", "admin", "RemeNUSTWH")
+print client.connected
--- _DEVELOPMENT/messybot/slackbot_settings-example.py
+++ _DEVELOPMENT/messybot/slackbot_settings-example.py
@@ -1,2 +1,14 @@
 API_TOKEN = "TOKEN HERE"
+ERRORS_TO = "username"
+
+PLUGINS = [
+    'plugins.repeat',
+    'plugins.testing',
+    'plugins.zwave'
+]
+
+PYISY_ADDR = "123.123.123.123"
+PYISY_PORT = "80"
+PYISY_USER = "useruserexample"
+PYISY_PASS = "passpassexample"
 
