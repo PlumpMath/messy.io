0ed0fa02bfacbf0aacf0d8f7888c20daff49035b
Dan Taeyoung <provolot@gmail.com>
2016-11-23 09:52:49 -0500

    logintest

--- _DEVELOPMENT/messybot/logintest.py
+++ _DEVELOPMENT/messybot/logintest.py
@@ -1,3 +1,5 @@
+import slackbot_settings
 import PyISY
-client = PyISY.ISY("74.113.161.154", "1080", "admin", "RemeNUSTWH")
+print(slackbot_settings.PYISY_ADDR, slackbot_settings.PYISY_PORT, slackbot_settings.PYISY_USER, slackbot_settings.PYISY_PASS)
+client = PyISY.ISY(slackbot_settings.PYISY_ADDR, slackbot_settings.PYISY_PORT, slackbot_settings.PYISY_USER, slackbot_settings.PYISY_PASS)
 print client.connected
